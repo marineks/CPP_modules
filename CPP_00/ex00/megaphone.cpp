1a78f94f0e80fff3c7ffc4691c50f23aef7bba68
Marine SANJUAN <marine@sanjuan.eu>
2022-07-06 11:31:24 +0200

    Megaphone ex done

---
+++ CPP_00/ex00/megaphone.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/05 16:04:31 by msanjuan          #+#    #+#             */
+/*   Updated: 2022/07/06 11:26:18 by msanjuan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstring>
+
+int	main(int argc, char **argv)
+{
+	if (argc >= 2)
+	{
+		for (int i = 1; argv[i]; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+			{
+				char c = argv[i][j];
+				std::cout << (char)toupper(c);
+			}
+			std::cout << " ";
+		}
+		std::cout << std::endl;
+	}
+	else 
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return (0);
+}
+
