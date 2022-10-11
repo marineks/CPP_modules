59469831969ef38358389b3b435e91126cee787d
Marine SANJUAN <marine@sanjuan.eu>
2022-10-11 21:58:18 +0200

    ex02 done no leaks

--- CPP_07/ex02/Array.hpp
+++ CPP_07/ex02/Array.hpp
@@ -3,16 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marine <marine@student.42.fr>              +#+  +:+       +#+        */
+/*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 00:39:58 by marine            #+#    #+#             */
-/*   Updated: 2022/09/27 00:40:22 by marine           ###   ########.fr       */
+/*   Updated: 2022/10/11 21:36:42 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
-
 
 # define RESET "\033[0m"
 # define BLACK "\033[0;30m"
@@ -24,5 +23,139 @@
 # define CYAN "\033[0;36m"
 # define WHITE "\033[0;37m"
 
+#include <string>
+#include <iostream>
+
+template<typename T>
+class Array
+{
+	public:
+			Array<T>(void);					// default
+			Array<T>(Array<T> const &src);	// copy
+			~Array<T>(void);				// destructor
+			Array<T>		&operator=(Array<T> const &rhs);
+
+			/* Parametric constructor */
+			Array<T>(unsigned int n);
+			
+			/* Accessors */
+			unsigned int	 size(void) const;
+			T				*getContent(void) const;
+			
+			/* Operator overload */
+			T				&operator[](unsigned int index);
+			
+			
+	private:
+			unsigned int	_n; // nombre d'éléments du tableau
+			T			   *_content;
+};
+
+
+class InvalidIndexException : public std::exception
+{
+	public:
+		virtual const char *what() const throw()
+		{
+			return ("The index is not correct");
+		};
+};
+
+
+
+/*****
+****** CONSTRUCTORS AND DESTRUCTORS
+******
+*********************************************************/
+
+/* ---------------- CANONICAL -----------------*/
+template<typename T>
+Array<T>::Array(void) : _n(0), _content(0) {
+	std::cout << "[DEFAULT CONSTRUCTOR] " << CYAN << "An empty array was created" << RESET << std::endl;
+}
+
+template<typename T>
+Array<T>::Array(Array<T> const &src) : _n(0)
+{
+	*this = src;
+	std::cout << "[COPY CONSTRUCTOR] " << PURPLE << "A copy of an array was created" << RESET << std::endl;
+}
+
+template<typename T>
+Array<T>::~Array<T>(void)
+{
+	delete [] this->_content;
+	std::cout << "[DESTRUCTOR] " << BLUE << "An instance of an array was destroyed" << RESET << std::endl;
+}
+
+
+template<typename T>
+Array<T>	&Array<T>::operator=(Array<T> const &rhs)
+{
+	std::cout << "[ASSIGNATION OVERLOAD] " << YELLOW << " An array is being assigned to another" << RESET << std::endl;
+	
+	/* Avant de "remplir", vider ce qu'il y a déjà */
+	if (this->_n > 0)
+		delete [] this->_content;
+	
+	/* Allouer espace pour array */
+	T *copyOfContent = new T[rhs.size()];
+	
+	/* Y "ranger" le content de rhs */
+	for (unsigned int i = 0; i < rhs.size(); i++)
+		copyOfContent[i] = rhs._content[i];
+	
+	/* attribuer aux éléments de l'array actuel ceux "copiés" plus haut */
+	this->_n = rhs.size();
+	this->_content = copyOfContent;
+	
+	return (*this);
+}
+
+
+/*****
+****** PARAMETRIC CONSTRUCTOR
+******
+*********************************************************/
+template<typename T>
+Array<T>::Array(unsigned int n) : _n(n), _content(new T[n]) {
+	// this->_n = n;
+	// this->_content = new T[n];
+	std::cout << "[PARAMETRIC CONSTRUCTOR] " << GREEN << "An array of size n: " << n << " was created" << RESET << std::endl;
+}
+
+
+/*****
+****** ACCESSORS
+******
+*********************************************************/
+template<typename T>
+unsigned int	 Array<T>::size(void) const { return (this->_n); }
+
+template<typename T>
+T				*Array<T>::getContent(void) const { return (this->_content); }
+
+
+/*****
+****** OTHER OPERATOR OVERLOADS
+******
+*********************************************************/
+
+template<typename T>
+T	&Array<T>::operator[](unsigned int index)
+{
+	if (index >= this->_n || index < 0)
+		throw InvalidIndexException();
+	return (this->_content[index]);
+}
+
+template<typename T>
+std::ostream	&operator<<(std::ostream &stream, Array<T> const &array)
+{
+	for (size_t i = 0; i < array.size(); i++)
+		stream << "Index [" << CYAN << i << RESET << "] = " << array.getContent()[i] << std::endl;
+
+	return (stream);
+}
 
 #endif
--- CPP_07/ex02/main.cpp
+++ CPP_07/ex02/main.cpp
@@ -3,17 +3,87 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marine <marine@student.42.fr>              +#+  +:+       +#+        */
+/*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 00:41:04 by marine            #+#    #+#             */
-/*   Updated: 2022/09/27 00:41:36 by marine           ###   ########.fr       */
+/*   Updated: 2022/10/11 21:57:31 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
+#include <iostream>
+#include <cstdlib>
 
-int main(int argc, const char** argv)
-{
+#define MAX_VAL 9999
+
+int main(void)
+{	
+
+	Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
 	
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+   
+    //SCOPE
+    {
+		std::cout << "Scope" << std::endl;
+        Array<int> tmp = numbers;
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			if (tmp[i] != numbers[i])
+			{
+				std::cerr << "ERROR : didn't save the same value!!" << std::endl;
+				return (1);
+			}
+		}
+        Array<int> test(tmp);
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			if (test[i] != numbers[i])
+			{
+				std::cerr << "ERROR : didn't save the same value!!" << std::endl;
+				return (1);
+			}
+		}
+		test[1] = 4;
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			if (test[i] != numbers[i])
+			{
+				std::cerr << "ERROR : didn't save the same value!!" << std::endl; // devrait être triggered
+			}
+		}
+    }
+
+	// ON CHECK QUE NOTRE ARRAY A LE MEME COMPORTEMENT QUE MIRROR
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "ERROR : didn't save the same value!!" << std::endl;
+            return (1);
+        }
+    }
+    
+	// 2 CAS QUI SONT SENSES TRIGGER DES EXCEPTIONS (range : [0, MAX_VAL - 1])
+	try { numbers[-2] = 0; }
+    catch(const std::exception& e) {  std::cerr << e.what() << '\n'; }
+    
+	try { numbers[MAX_VAL] = 0; }
+    catch(const std::exception& e) {  std::cerr << e.what() << '\n'; }
+
+    for (int i = 0; i < MAX_VAL; i++)
+	{
+		numbers[i] = rand();
+	}
+    
+	delete [] mirror;
+
 	return (0);
 }
